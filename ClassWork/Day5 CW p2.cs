de293e6c7260571b2f07a085fef456e113e4008e
Ali <112066046+MuhdAliSajjad@users.noreply.github.com>
2022-12-30 17:52:05 +0800

    Add files via upload

---
+++ ClassWork/Day5 CW p2.cs
@@ -0,0 +1,61 @@
+﻿//using System;
+//using System.Linq;
+
+//namespace LINQExample
+//{
+//    class Program
+//    {
+//        static void Main(string[] args)
+//        {
+//            // Define an array of integers
+//            //int[] nums = { 1, 2, 3, 3, 3, 1, 4, 12, 1, 2 }; // own test case  
+
+//            /////////////////////////////////////////////////////////////////////////////
+//            //Q1. Use LINQ to find the numbers of frequenciy
+//            int[] nums = { 1, 2, 3, 1, 2, 1, 0 }; // test case
+//            var groups = nums.GroupBy(x => x);
+//            foreach (var i in groups)
+//            {
+//                Console.WriteLine("Value: {0}, Frequency: {1}", i.Key, i.Count());
+//            }
+//            // sirs method for Q1
+//            var number2 = from num in nums group num by num into newnum select newnum;
+//            foreach (var i in number2)
+//            {
+//                Console.WriteLine($"{i.Key} --> {i.Count()}");
+//            }
+//            /////////////////////////////////////////////////////////////////////////////
+
+//            /////////////////////////////////////////////////////////////////////////////
+//            //Q2 LINQSquares question
+//            int[] numbers = { 1, 2, 3, 42, 35, 56, 17, 48, 19, 120 };
+//            var numbersAndPower = from n in numbers
+//                                  where n > 20
+//                                  select new
+//                                  {
+//                                      Number = n,
+//                                      Square = n * n
+//                                  };
+//            // Print the Square output
+//            foreach (var i in numbersAndPower)
+//            {
+//                Console.WriteLine($"Number:{i.Number}, Square: {i.Square}");
+//            }
+//            /////////////////////////////////////////////////////////////////////////////
+
+//            /////////////////////////////////////////////////////////////////////////////
+//            // Q3 LINQDivBy2
+//            //Print the Div 2 Output
+//            int[] number3 = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+//            var numbersAndDiv = from n in number3
+//                                where n % 2 == 0
+//                                select n;
+
+//            foreach (var i in numbersAndDiv)
+//            {
+//                Console.WriteLine($"Number: {i}");
+//            }
+//            /////////////////////////////////////////////////////////////////////////////
+//        }
+//    }
+//}
---
+++ ClassWork/Day5 CW.cs
@@ -0,0 +1,44 @@
+﻿//using System;
+
+//namespace StockProfit
+//{
+//    class Program
+//    {
+
+
+//        public static int MaxProfit(int[] prices)
+//        {
+//            // Edge case: if the array has fewer than 2 elements, we can't make a profit
+//            if (prices.Length < 2)
+//            {
+//                return 0;
+//            }
+
+//            // Initialize the minimum price and maximum profit to the first element in the array
+//            int minPrice = prices[0];
+//            int maxProfit = 0;
+
+//            // Iterate through the rest of the array and update the minimum price and maximum profit as needed
+//            for (int i = 1; i < prices.Length; i++)
+//            {
+//                // Update the minimum price if we find a new lowest price
+//                minPrice = Math.Min(minPrice, prices[i]);
+
+//                // Update the maximum profit if we find a new highest profit
+//                maxProfit = Math.Max(maxProfit, prices[i] - minPrice);
+//            }
+
+//            return maxProfit;
+//        }
+
+//        static void Main(string[] args)
+//        {
+//            // Test cases
+//            int[] prices1 = { 7, 1, 5, 3, 6, 4 };
+//            int[] prices2 = { 7, 6, 4, 3, 1 };
+
+//            Console.WriteLine("The maximum profit for first price: " + MaxProfit(prices1)); // should output 5
+//            Console.WriteLine("The maximum profit for second price: " + MaxProfit(prices2)); // should output 0
+//        }
+//    }
+//}
